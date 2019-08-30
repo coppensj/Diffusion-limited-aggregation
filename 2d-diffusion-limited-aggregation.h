0251759afdcbb058fa8fd8af329b059f344ec976
jcoppens <jcoppens@ghgsat.com>
2019-08-30 12:04:30 -0400

    Switches walker from a pair of doubles to Vec2D object

--- 2d-diffusion-limited-aggregation.h
+++ 2d-diffusion-limited-aggregation.h
@@ -1,11 +1,7 @@
 #include <random>
 #include <cmath>
 #include <stdio.h>
-
-/* template <typename T, typename U> */
-/* std::pair<T,U> operator+(const std::pair<T,U> &l, const std::pair<T,U> & r) { */
-/*         return{l.first+r.first, l.second+r.second}; */
-/*         } */
+#include "Vec2D.h"
 
 class DLA_2d {
 	//Data
@@ -15,20 +11,19 @@ class DLA_2d {
 		std::random_device rd;
 		std::mt19937 generator;
 		std::normal_distribution<double> movement;
-        std::pair<double,double> walkerPosition;
+        Vec<double> walker;
 		
 	//Methods
     public:
         DLA_2d(): 
             movement(0, 1), 
 			generator(std::mt19937(rd())),
-            walkerPosition(0,0) { }
+            walker(0,0) { }
 
         void SingleStep(){
             for(int i=0; i<20; ++i){
-                printf("%5.2f, %5.2f\n", walkerPosition.first, walkerPosition.second);
-                walkerPosition.first += movement(generator);
-                walkerPosition.second += movement(generator);
+                printf("%5.2f, %5.2f\n", walker.x, walker.y);
+                MoveWalker();
             }
         }
 	private:
@@ -37,7 +32,8 @@ class DLA_2d {
 		}
 
 		void MoveWalker(){
-
+            walker.x += movement(generator);
+            walker.y += movement(generator); 
 		}
 
 		void AddToCluster(){
